b0dd6313beed67e6d782452c2bd12650221cbc5d
littus <athlonnus1@gmail.com>
2023-01-10 19:13:58 +0200

    Case Study Internal Page front--DONE+

--- wp-content/themes/maxcanvas_child/single-cases.php
+++ wp-content/themes/maxcanvas_child/single-cases.php
@@ -22,7 +22,7 @@ $get_queried_object = get_queried_object();
 					<div class="row">
 						<div class="col-sm col-12 mb-sm-4 mb-3">
 							<div class="d-flex align-items-start">
-								<img src="http://maxplaces.name/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
+								<img src="https://maxplacesnew.maxdroplet1.maxburst.dev/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
 								<div class="ms-3">
 									<p class="text-uppercase mb-1"> <strong>Local search Marketing</strong> </p>
 									<p class="mb-0">Managing your online reputation isn’t shameless promotion.</p>
@@ -31,26 +31,7 @@ $get_queried_object = get_queried_object();
 						</div>
 						<div class="col-sm col-12 mb-sm-4 mb-3">
 							<div class="d-flex align-items-start">
-								<img src="http://maxplaces.name/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
-								<div class="ms-3">
-									<p class="text-uppercase mb-1"> <strong>Local search Marketing</strong> </p>
-									<p class="mb-0">Managing your online reputation isn’t shameless promotion.</p>
-								</div>
-							</div>
-						</div>
-						<div class="w-100"></div>
-						<div class="col-sm col-12 mb-sm-4 mb-3">
-							<div class="d-flex align-items-start">
-								<img src="http://maxplaces.name/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
-								<div class="ms-3">
-									<p class="text-uppercase mb-1"> <strong>Local search Marketing</strong> </p>
-									<p class="mb-0">Managing your online reputation isn’t shameless promotion.</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm col-12 mb-sm-4 mb-3">
-							<div class="d-flex align-items-start">
-								<img src="http://maxplaces.name/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
+								<img src="https://maxplacesnew.maxdroplet1.maxburst.dev/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
 								<div class="ms-3">
 									<p class="text-uppercase mb-1"> <strong>Local search Marketing</strong> </p>
 									<p class="mb-0">Managing your online reputation isn’t shameless promotion.</p>
@@ -60,7 +41,7 @@ $get_queried_object = get_queried_object();
 						<div class="w-100"></div>
 						<div class="col-sm col-12 mb-sm-4 mb-3">
 							<div class="d-flex align-items-start">
-								<img src="http://maxplaces.name/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
+								<img src="https://maxplacesnew.maxdroplet1.maxburst.dev/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
 								<div class="ms-3">
 									<p class="text-uppercase mb-1"> <strong>Local search Marketing</strong> </p>
 									<p class="mb-0">Managing your online reputation isn’t shameless promotion.</p>
@@ -69,7 +50,26 @@ $get_queried_object = get_queried_object();
 						</div>
 						<div class="col-sm col-12 mb-sm-4 mb-3">
 							<div class="d-flex align-items-start">
-								<img src="http://maxplaces.name/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
+								<img src="https://maxplacesnew.maxdroplet1.maxburst.dev/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
+								<div class="ms-3">
+									<p class="text-uppercase mb-1"> <strong>Local search Marketing</strong> </p>
+									<p class="mb-0">Managing your online reputation isn’t shameless promotion.</p>
+								</div>
+							</div>
+						</div>
+						<div class="w-100"></div>
+						<div class="col-sm col-12 mb-sm-4 mb-3">
+							<div class="d-flex align-items-start">
+								<img src="https://maxplacesnew.maxdroplet1.maxburst.dev/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
+								<div class="ms-3">
+									<p class="text-uppercase mb-1"> <strong>Local search Marketing</strong> </p>
+									<p class="mb-0">Managing your online reputation isn’t shameless promotion.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm col-12 mb-sm-4 mb-3">
+							<div class="d-flex align-items-start">
+								<img src="https://maxplacesnew.maxdroplet1.maxburst.dev/wp-content/uploads/2022/10/services-icon_3.svg" alt="">
 								<div class="ms-3">
 									<p class="text-uppercase mb-1"> <strong>Local search Marketing</strong> </p>
 									<p class="mb-0">Managing your online reputation isn’t shameless promotion.</p>
@@ -85,7 +85,7 @@ $get_queried_object = get_queried_object();
 
 	<section class="s-c-p--2-section industry-served-page--f-st mt-md-5 mt-0 mb-md-0 mb-3">
 		<div class="container-lg --correct">
-			<div class="industry-served-page--f-st--bg" style="background-image: url(http://maxplaces.name/wp-content/uploads/2022/12/istockphoto-850495466-2048x2048-1.jpg)"></div>
+			<div class="industry-served-page--f-st--bg" style="background-image: url(https://maxplacesnew.maxdroplet1.maxburst.dev/wp-content/uploads/2022/12/istockphoto-850495466-2048x2048-1.jpg)"></div>
 			<aside style="padding-bottom:0;">
 				<div class="row">
 					<div class="col-sm-6 col-12">
@@ -111,18 +111,18 @@ $get_queried_object = get_queried_object();
 					<img class="img-fluid" src="https://www.maxburst.com/wp-content/uploads/2021/11/the_problem_section_mockup580px-1.jpg" alt="">
 				</div>
 				<div class="order-md-2 order-1">
-					<div style="position:relative;">
-						<img src="<?php echo get_stylesheet_directory_uri();?>/img/case-study-internal-svg-icon.svg" alt=""> <span style="font-weight:600; font-size:15px; position:absolute; left:0; top:20px;">01</span>
+					<div class="item-slot position-relative">
+						<img src="<?php echo get_stylesheet_directory_uri();?>/img/case-study-internal-svg-icon.svg" alt=""> <span>01</span>
 						<h3>The Problem:</h3>
 						<p>We determined that a complete redesign for GarageTek was necessary with attention not only to the design but also to content detail. Within that redesign, we would place a strong concentration on powerful, intuitive tools for advanced content management and e-commerce capabilities. We wanted to emphasize content placement and messaging strategies while including strong call-to-action elements for lead generation and e-com capabilities.</p>
 					</div>
-					<div style="position:relative;">
-						<img src="<?php echo get_stylesheet_directory_uri();?>/img/case-study-internal-svg-icon.svg" alt=""> <span style="font-weight:600; font-size:15px; position:absolute; left:0; top:20px;">02</span>
+					<div class="item-slot position-relative">
+						<img src="<?php echo get_stylesheet_directory_uri();?>/img/case-study-internal-svg-icon.svg" alt=""> <span>02</span>
 						<h3>The Solution:</h3>
 						<p>We determined that a complete redesign for GarageTek was necessary with attention not only to the design but also to content detail. Within that redesign, we would place a strong concentration on powerful, intuitive tools for advanced content management and e-commerce capabilities. We wanted to emphasize content placement and messaging strategies while including strong call-to-action elements for lead generation and e-com capabilities.</p>
 					</div>
-					<div style="position:relative;">
-						<img src="<?php echo get_stylesheet_directory_uri();?>/img/case-study-internal-svg-icon.svg" alt=""> <span style="font-weight:600; font-size:15px; position:absolute; left:0; top:20px;">03</span>
+					<div class="item-slot position-relative">
+						<img src="<?php echo get_stylesheet_directory_uri();?>/img/case-study-internal-svg-icon.svg" alt=""> <span>03</span>
 						<h3>The Results:</h3>
 						<p>We determined that a complete redesign for GarageTek was necessary with attention not only to the design but also to content detail. Within that redesign, we would place a strong concentration on powerful, intuitive tools for advanced content management and e-commerce capabilities. We wanted to emphasize content placement and messaging strategies while including strong call-to-action elements for lead generation and e-com capabilities.</p>
 					</div>
@@ -135,7 +135,8 @@ $get_queried_object = get_queried_object();
 		<div class="container-lg">
 			<div class="row row-cols-lg-2 row-cols-md-2 row-cols-1 d-flex align-items-center align-self-center">
 				<div class="order-md-1 order-1">
-					<h2>The Analysis:</h2>
+					<div class="sup-title text-uppercase mb-3">GarageTek</div>
+					<h2>Digital Marketing Maximized:</h2>
 					<p>We determined that a complete redesign for GarageTek was necessary with attention not only to the design but also to content detail. Within that redesign, we would place a strong concentration on powerful, intuitive tools for advanced content management and e-commerce capabilities. We wanted to emphasize content placement and messaging strategies while including strong call-to-action elements for lead generation and e-com capabilities.</p>
 				</div>
 				<div class="order-md-2 mt-md-0 mt-4 order-2 text-md-start text-center">
